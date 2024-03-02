18504fc788508464cbc2d7689d94199a0d519a5f
000itsuki000 <itsuki.h@collabo-style.co.jp>
2024-03-02 17:01:07 +0900

    Initial Commit

---
+++ AWSIoTDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AWSIoTDemo
+//
+//  Created by Itsuki on 2024/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
